4efea2bdde5ca9b8d9223584c9e063d296ac3492
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-16 21:25:34 +0200

    03 nextjs essentials: reserved filenames and project structure discussion

--- 03-nextjs-essentials/src/app/page.tsx
+++ 03-nextjs-essentials/src/app/page.tsx
@@ -1,17 +1,10 @@
-import Image from "next/image";
+import Header from "@/components/Header";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Image
-        src="/icon.png"
-        width={400}
-        height={400}
-        alt="A server surrounded by magic sparkles."
-      />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <Header />
       <p>
         <Link href="/about">About Us</Link>
       </p>
