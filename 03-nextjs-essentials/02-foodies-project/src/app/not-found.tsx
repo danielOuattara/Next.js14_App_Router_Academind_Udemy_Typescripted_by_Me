72fac6b473b46314fc4130e2fff739fc3558777e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-22 16:19:39 +0200

    03 nextjs essentials: 02 foodies: not-found page at root level

---
+++ 03-nextjs-essentials/02-foodies-project/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>404 Not Found</h1>
+      <p>The page you requested is unknown</p>
+    </main>
+  );
+}
