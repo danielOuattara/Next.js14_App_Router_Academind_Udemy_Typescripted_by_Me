bd6e157240a6addacc3d02ff24501ef1a223e223
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-24 23:52:25 +0200

    03 nextjs essentials: 02 foodies:  slugify + xss instructions

--- 03-nextjs-essentials/02-foodies-project/src/libraries/meal.ts
+++ 03-nextjs-essentials/02-foodies-project/src/libraries/meal.ts
@@ -1,4 +1,6 @@
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
 
 const db = sql("meals.db");
 
@@ -15,4 +17,10 @@ export async function getSingleMeal(slug: string): Promise<TypeMeal> {
   // await new Promise((resolve) => setTimeout(resolve, 3000));
   // throw new Error("Something went wrong ! ");
   return db.prepare("SELECT * FROM meals WHERE slug= ?").get(slug) as TypeMeal;
+}
+
+export async function saveMeal(meal: TypeMeal) {
+  meal.slug = slugify(`${meal.title}-${meal.creator}`, { lower: true });
+
+  meal.instructions = xss(meal.instructions);
 }
